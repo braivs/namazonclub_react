7888b403992ebede9948090118147e5ce56f1e9e
Briws <brightwiths@gmail.com>
2023-04-28 23:11:25 +0530

    Footer updated

--- src/components/UniverseEng/Footer/Footer.tsx
+++ src/components/UniverseEng/Footer/Footer.tsx
@@ -1,19 +1,22 @@
 import React from "react"
 import s from "./Footer.module.scss"
+import socialFacebook from 'src/img/socialFacebook.png'
+import socialVK from 'src/img/socialVk.png'
+import socialTelegramColor from 'src/img/socialTelegramColor.png'
 
 export function Footer() {
     return (
         <footer className={s.footer}>
             <div className={s.socialBox}>
-                <a href="https://www.facebook.com/groups/namazonclub"><img src="img/socialFacebook.png" alt=""/></a>
-                <a href="https://vk.com/namazon"><img src="img/socialVk.png" alt=""/></a>
+                <a href="https://www.facebook.com/groups/namazonclub"><img src={socialFacebook} alt=""/></a>
+                <a href="https://vk.com/namazon"><img src={socialVK} alt=""/></a>
             </div>
             <div className="row">
                 <div className="col-12 text-center">
                     <p>©2021 Club of single combats for women "Namazon"</p>
                     <p className={s.socialMy}><span>Site created by BriWS</span><span><a
-                        href="https://t.me/brightwiths"><img
-                        src="img/social-telegram-color.png" alt=""/></a></span><span>2020</span></p>
+                        href="https://t.me/brightwithsme"><img
+                        src={socialTelegramColor} alt=""/></a></span><span>2023</span></p>
                 </div>
             </div>
         </footer>
