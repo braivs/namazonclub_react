bc903a9dc906dd75a2ad01567c00a958ff86ade8
Briws <brightwiths@gmail.com>
2023-05-25 05:34:33 +0530

    added more data to cardsWrestling.ts

--- src/localdata/cardsWrestling.ts
+++ src/localdata/cardsWrestling.ts
@@ -4,6 +4,22 @@ import video42 from 'src/img/video/wrestThumb/video-42.jpg'
 import video41 from 'src/img/video/wrestThumb/video-41.jpg'
 import video37 from 'src/img/video/wrestThumb/video-37.jpg'
 import video36 from 'src/img/video/wrestThumb/video-36.jpg'
+import video35 from 'src/img/video/wrestThumb/video-35.jpg'
+import video33 from 'src/img/video/wrestThumb/video-33.jpg'
+import video29 from 'src/img/video/wrestThumb/video-29.jpg'
+import video27 from 'src/img/video/wrestThumb/video-27.jpg'
+import video26 from 'src/img/video/wrestThumb/video-26.jpg'
+import video24 from 'src/img/video/wrestThumb/video-24.jpg'
+import video23 from 'src/img/video/wrestThumb/video-24.jpg'
+import video19 from 'src/img/video/wrestThumb/video-19.jpg'
+import video16 from 'src/img/video/wrestThumb/video-16.jpg'
+import video12 from 'src/img/video/wrestThumb/video-12.jpg'
+import video8 from 'src/img/video/wrestThumb/video-8.jpg'
+import video6 from 'src/img/video/wrestThumb/video-6.jpg'
+import video4 from 'src/img/video/wrestThumb/video-4.jpg'
+import video3 from 'src/img/video/wrestThumb/video-3-ww.jpg'
+import video2 from 'src/img/video/wrestThumb/video-2.jpg'
+import video1 from 'src/img/video/wrestThumb/video-1.jpg'
 
 export const cardsWrestling: Array<CardType> = [
     {
@@ -35,5 +51,101 @@ export const cardsWrestling: Array<CardType> = [
         linkRef: '/videos/36',
         imgRef: video36,
         alt: 'video-36',
+    },
+    {
+        title: 'VIDEO 35<br>Lidiya Oslopovskih vs Tais<br>Pins matches. 2013',
+        linkRef: '/videos/35',
+        imgRef: video35,
+        alt: 'video-35',
+    },
+    {
+        title: 'VIDEO 33<br>Crossfit tournament<br>Submission Grappling. 2013',
+        linkRef: '/videos/33',
+        imgRef: video33,
+        alt: 'video-33',
+    },
+    {
+        title: 'VIDEO 29<br>Elena Vasilyeva vs Tais<br>Submission Grappling. 2013',
+        linkRef: '/videos/29',
+        imgRef: video29,
+        alt: 'video-29',
+    },
+    {
+        title: 'VIDEO 27<br>Female Beach Wrestling<br>Part 2. June, 2011',
+        linkRef: '/videos/27',
+        imgRef: video27,
+        alt: 'video-27',
+    },
+    {
+        title: 'VIDEO 26<br>Female Beach Wrestling<br>Part 2. June, 2011',
+        linkRef: '/videos/26',
+        imgRef: video26,
+        alt: 'video-26',
+    },
+    {
+        title: 'VIDEO 24<br>Lyudmila vs Tais<br>Submission Grappling. 2011',
+        linkRef: '/videos/24',
+        imgRef: video24,
+        alt: 'video-24',
+    },
+    {
+        title: 'VIDEO 23<br>Irina (Vlasta) vs Tais<br>Submission Grappling. 2011',
+        linkRef: '/videos/23',
+        imgRef: video23,
+        alt: 'video-23',
+    },
+    {
+        title: 'VIDEO 19<br>Varvara Akulova vs Tais<br>Submission Grappling. 2012',
+        linkRef: '/videos/19',
+        imgRef: video19,
+        alt: 'video-19',
+    },
+    {
+        title: 'VIDEO 16<br>Elena Vasilyeva vs Tais<br>Submission Grappling. 2011',
+        linkRef: '/videos/16',
+        imgRef: video16,
+        alt: 'video-16',
+    },
+    {
+        title: 'VIDEO 12<br>Submission Grappling<br>Tournament.  April,  2010',
+        linkRef: '/videos/12',
+        imgRef: video12,
+        alt: 'video-12',
+    },
+    {
+        title: 'VIDEO 8<br>Women\'s Beach Tournament<br>Submission Grappling.  2011',
+        linkRef: '/videos/8',
+        imgRef: video8,
+        alt: 'video-8',
+    },
+    {
+        title: 'VIDEO 6<br>Maria Rylyova  vs Tais<br>Armwrestling and Wrestling',
+        linkRef: '/videos/6',
+        imgRef: video6,
+        alt: 'video-6',
+    },
+    {
+        title: 'VIDEO 4<br>Submission Grappling<br>"Christmas Cup  2011',
+        linkRef: '/videos/4',
+        imgRef: video4,
+        alt: 'video-4',
+    },
+    {
+        title: 'VIDEO 3<br>Beach Wrestling<br>Mixed Tournament.  2010',
+        linkRef: '/videos/3',
+        imgRef: video3,
+        alt: 'video-3',
+    },
+    {
+        title: 'VIDEO 2<br>Submission Grappling<br>Part 2.  June,  2010',
+        linkRef: '/videos/2',
+        imgRef: video2,
+        alt: 'video-2',
+    },
+    {
+        title: 'VIDEO 1<br>Submission Grappling<br>Part 1.  June,  2010',
+        linkRef: '/videos/1',
+        imgRef: video1,
+        alt: 'video-1',
     },
 ]
