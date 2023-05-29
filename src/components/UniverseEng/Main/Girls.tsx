c7c5f0ee206bb502fc66909ecb6ad3a7371675ab
Briws <brightwiths@gmail.com>
2023-05-29 18:59:20 +0530

    Girls useParams selection complete

--- src/components/UniverseEng/Main/Girls.tsx
+++ src/components/UniverseEng/Main/Girls.tsx
@@ -2,22 +2,25 @@ import React from 'react'
 import sC from "../../../common/styles.module.scss"
 import {Cards} from "../../../common/Cards/Cards"
 import {cardsGirls2010_2011, cardsGirls2012_2013} from "../../../localdata/cardsGirls"
+import {useParams} from "react-router-dom"
 
 export const Girls = () => {
+    const {cardsVariant} = useParams()
+
     return (
         <>
             <div className={sC.header}>
                 <h3>Participants of competitions</h3>
                 <h3>Subcategories</h3>
             </div>
-            <div>
+            {(cardsVariant === 'all' || cardsVariant === '2012-2013') && <div>
               <p>2012 - 2013</p>
               <Cards cardsArray={cardsGirls2012_2013} variant={"girls"}/>
-            </div>
-            <div>
+            </div> }
+            {(cardsVariant === 'all' || cardsVariant === '2010-2011') && <div>
                 <p>2010-2011</p>
                 <Cards cardsArray={cardsGirls2010_2011} variant={"girls"}/>
-            </div>
+            </div>  }
         </>
     )
 }
