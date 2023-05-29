b5a1a9a227063f1c6a655ed63443b8e4f939816d
Briws <brightwiths@gmail.com>
2023-05-29 21:48:40 +0530

    Complete Competitions page

--- src/components/UniverseEng/Main/Competitions.tsx
+++ src/components/UniverseEng/Main/Competitions.tsx
@@ -1,19 +1,44 @@
 import React from 'react'
 import sC from "../../../common/styles.module.scss"
 import {Cards} from "../../../common/Cards/Cards"
-import {cardsCompetitions2015_2017} from "../../../localdata/cardsCompetitions"
+import {
+    cardsArchive,
+    cardsCompetitions2011,
+    cardsCompetitions2012,
+    cardsCompetitions2013_2014,
+    cardsCompetitions2015_2017
+} from "../../../localdata/cardsCompetitions"
+import {useParams} from "react-router-dom"
 
 export const Competitions = () => {
+    const {cardsVariant} = useParams()
+
     return (
         <>
             <div className={sC.header}>
                 <h3>Results of the past competitions</h3>
                 <h3>Subcategories</h3>
             </div>
-            <div>
+            {(cardsVariant === 'all' || cardsVariant === '2015-2017') && <div>
               <p>2015-2017</p>
               <Cards cardsArray={cardsCompetitions2015_2017} variant={'competitions'}/>
-            </div>
+            </div> }
+            {(cardsVariant === 'all' || cardsVariant === '2013-2014') && <div>
+                <p>2013-2014</p>
+                <Cards cardsArray={cardsCompetitions2013_2014} variant={'competitions'}/>
+            </div> }
+            {(cardsVariant === 'all' || cardsVariant === '2012') && <div>
+                <p>2012</p>
+                <Cards cardsArray={cardsCompetitions2012} variant={'competitions'}/>
+            </div> }
+            {(cardsVariant === 'all' || cardsVariant === '2011') && <div>
+                <p>2011</p>
+                <Cards cardsArray={cardsCompetitions2011} variant={'competitions'}/>
+            </div>}
+            {(cardsVariant === 'all' || cardsVariant === 'Archive') && <div>
+                <p>Archive</p>
+                <Cards cardsArray={cardsArchive} variant={'competitions'}/>
+            </div> }
         </>
     )
 }
--- src/localdata/cardsCompetitions.ts
+++ src/localdata/cardsCompetitions.ts
@@ -1,5 +1,21 @@
 import {CardType} from "../common/types"
 import maslenitsa_2016 from "../img/competitions/maslenitsa-2016.jpg"
+import tournament_2013 from "../img/competitions/tournament-2013.jpg"
+import tournament_2014 from "../img/competitions/tournament-2014.jpg"
+import grappling_2014 from "../img/competitions/grappling-2014.jpg"
+import Elena_Vasilyeva_vs_Kara_Teller from '../img/competitions/Elena_Vasilyeva_vs_Kara_Teller.jpg'
+import Kara_Teller_vs_Svetlana_Solovyova from '../img/competitions/Kara_Teller_vs_Svetlana_Solovyova.jpg'
+import KaraTellervsDariaBalina from '../img/competitions/KaraTellervsDariaBalina.jpg'
+import VarvaravsTais from '../img/competitions/VarvaravsTais.jpg'
+import video_20 from '../img/competitions/video-20.jpg'
+import video_4 from '../img/competitions/video-4.jpg'
+import video_8 from '../img/competitions/video-8.jpg'
+import alex_elena_2011 from '../img/competitions/2011-alex-elena.jpg'
+import izida_tais_2008 from '../img/competitions/2008-izida-tais.jpg'
+import izida_irina_2008 from '../img/competitions/2008-izida-irina.jpg'
+import irina_tais_2009 from '../img/competitions/2009-irina-tais.jpg'
+import natalia_alex_2010 from '../img/competitions/2010-natalia-alex.jpg'
+
 
 
 export const cardsCompetitions2015_2017: Array<CardType> = [
@@ -7,5 +23,92 @@ export const cardsCompetitions2015_2017: Array<CardType> = [
         id: 'maslenitsa-2016',
         title: 'Female power competitions for\nthe Maslenitsa week, 2016',
         imgRef: maslenitsa_2016,
+    },
+]
+
+export const cardsCompetitions2013_2014: Array<CardType> = [
+    {
+        id: 'tournament-2013',
+        title: 'Women\'s Power Event\n Tournament. October 2013',
+        imgRef: tournament_2013,
+    },
+    {
+        id: 'tournament-2014',
+        title: 'Wrestling tournament for\nbeginners. May, 2014',
+        imgRef: tournament_2014,
+    },
+    {
+        id: 'grappling-2014',
+        title: 'Tournament for the prize of the\nNamazon Club. October, 2014',
+        imgRef: grappling_2014,
+    },
+]
+
+export const cardsCompetitions2012: Array<CardType> = [
+    {
+        id: 'christmas-2012',
+        title: 'Christmas Cup 2012\nMMA fights',
+        imgRef: Elena_Vasilyeva_vs_Kara_Teller,
+    },
+    {
+        id: 'mma-2012',
+        title: 'ММА. Kara Teller vs\nSvetlana Solovyova. 2012',
+        imgRef: Kara_Teller_vs_Svetlana_Solovyova,
+    },
+    {
+        id: 'sportHoliday-2012',
+        title: 'MMA fights in the May\nholidays. 2012',
+        imgRef: KaraTellervsDariaBalina,
+    },
+    {
+        id: 'varvara-tais',
+        title: 'Varvara Akulova vs Tais.\nSubmission grappling. 2012',
+        imgRef: VarvaravsTais,
+    },
+    {
+        id: 'nevskaya-arena-2012',
+        title: 'The first Nevsky Arena. MMA\n tournament. 2012',
+        imgRef: video_20,
+    },
+]
+
+export const cardsCompetitions2011: Array<CardType> = [
+    {
+        id: 'christmas-2011',
+        title: 'Christmas Cup 2011 Submission Grappling',
+        imgRef: video_4,
+    },
+    {
+        id: 'beach-2011',
+        title: 'Beach tournament\nSubmission Grappling. 2011',
+        imgRef: video_8,
+    },
+    {
+        id: 'beach-mix-2011',
+        title: 'Mixed beach tournament\nSubmission Grappling. 2011',
+        imgRef: alex_elena_2011,
+    },
+]
+
+export const cardsArchive: Array<CardType> = [
+    {
+        id: 'IzidaVsTais2008',
+        title: 'Izida vs Tais\nWrestling. 2008',
+        imgRef: izida_tais_2008,
+    },
+    {
+        id: 'IzidaVsIrina2008',
+        title: 'Izida vs Irina\nWrestling. 2008',
+        imgRef: izida_irina_2008,
+    },
+    {
+        id: 'IrinaVsTais2009',
+        title: 'Irina vs Tais\nBeach wrestling. 2009',
+        imgRef: irina_tais_2009,
+    },
+    {
+        id: 'beach-mix-2010',
+        title: 'Mixed beach\ntournament. 2010',
+        imgRef: natalia_alex_2010,
     },
 ]
