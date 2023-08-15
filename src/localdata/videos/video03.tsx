5a925c2682cb1c36a4a0248bcd9265d2b178cd57
Briws <brightwiths@gmail.com>
2023-08-15 22:54:12 +0400

    video03 complete

--- src/localdata/videos/video03.tsx
+++ src/localdata/videos/video03.tsx
@@ -4,9 +4,9 @@ import sC from 'src/common/styles.module.scss'
 import {getGalleryPhotosFromRequire} from "../../common/helpers"
 import {Collapse} from "@mui/material"
 import {LightboxMy, PhotoAlbumMy} from "../../common/ModdedComponents/ModdedComponents"
-import mainImg from 'src/img/videos/04main.jpg'
+import mainImg from 'src/img/videos/03main.jpg'
 
-const images = require.context('src/img/videos/04', false)
+const images = require.context('src/img/videos/03/ww', false)
 
 export const Video03 = () => {
     const videoNumber = 3
@@ -18,18 +18,19 @@ export const Video03 = () => {
     return (
         <>
             <h3>VIDEO {videoNumber}</h3>
-            <h4>"Christmas Cup 2011". Tournament on Submission Grappling</h4>
+            <h4>Beach Wrestling. Mixed Tournament. September, 2010</h4>
             <img className={sC.videosMainImg} src={mainImg} alt="imgMain"/>
             <p>
-                In the tournament participated: <b>Natalia Mysik</b> (5'11" / 154 lb), <b>Ksenia Voloshina</b> (5'3" /
-                141 lb) and <b>Valentina Perfilyeva</b> (5'8" / 139 lb). A feature of the competition was a big
-                disparity in weight and height between the wrestlers.<br/>
-                One of them (Natalia) was much heavier and taller than the others. Using this advantage, she frequently
-                put her opponents in pinning holds, especially School Girl Pins, which she would then easily turn into
-                armlocks or chokes. One of these chokes even resulted in her opponent’s knockout.<br/>
-                Beyond the tournament, there was a prize fight between  Tais  (5'7" / 132 lb) and Natalia Mysik.
+                Female (Namazonian) wrestlers at the tournament were <b>Tais</b> (5’7” / 132 lb) and <b>Natalia
+                Mysik</b> (5’11” / 154 lb). Their male opponent was Alex (5’9” / 150 lb). Namely, due to the three
+                fights between Natalia and Tais, <b>VIDEO 3</b> can go in the <b>WRESTLING</b> section of the website,
+                where the <b>FEMALE WRESTLING</b> matches are presented.
             </p>
-            <p>If you like long pinning holds, head scissors and chokes in women’s fights, you will love this film.</p>
+            <p>In these fights the rules were <b>SUBMISSION</b> only, no time limits indicated. This time Tais wrestled
+                a woman who was not only heavier than her but physically stronger as well. The struggle was very tough!
+                Both women fought in an aggressive manner, trying to gain an upper position and apply a good pin or
+                submission hold. Especially dramatic were the first two fights during which the wrestlers struggled
+                mostly on wet sand and the advantages were changing.</p>
             <Button variant="primary" onClick={() => setOpen(!open)}>SCREENSHOTS</Button>
             <Collapse in={open}>
                 <PhotoAlbumMy layout="masonry" photos={photos} openCallback={setIndex}/>
@@ -40,3 +41,5 @@ export const Video03 = () => {
         </>
     )
 }
+
+//todo: this is not mixed part of tournament, maybe rename
