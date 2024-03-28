b4647ddbf3faa7d55070aa062cc9175738141b44
Briws <brightwiths@gmail.com>
2024-03-28 19:22:02 +0700

    refactored code duplication

--- src/localdata/competition/2013-2014/Tournament2013Main.tsx
+++ src/localdata/competition/2013-2014/Tournament2013Main.tsx
@@ -7,13 +7,23 @@ import {Tournament2013_3} from "./Tournament2013_3"
 export const Tournament2013Main = () => {
     const [pageValue, setPageValue] = useState(1)
 
+    const tournamentComponents: TournamentComponentMap = {
+        1: Tournament2013_1,
+        2: Tournament2013_2,
+        3: Tournament2013_3,
+    };
+
+    const ActiveTournamentComponent = tournamentComponents[pageValue]
+
     return (
         <>
             <h4>Women's Power Event Tournament. October, 2013</h4>
             <PageSelector activePage={pageValue} setActivePage={setPageValue}/>
-            { pageValue === 1 && <Tournament2013_1/> }
-            { pageValue === 2 && <Tournament2013_2/> }
-            { pageValue === 3 && <Tournament2013_3/> }
+            {ActiveTournamentComponent && <ActiveTournamentComponent />}
         </>
     )
 }
+
+type TournamentComponentMap = {
+    [key: number]: React.ElementType;
+};
