90b80295ae64fd043ff229cb16f9183bee54a51d
Briws <brightwiths@gmail.com>
2024-08-07 02:12:13 +0700

    All gumroad links changed to patreon

--- src/common/constants/shopLinks.ts
+++ src/common/constants/shopLinks.ts
@@ -1,15 +1,11 @@
 export const LINK_SHOP_MAIN = 'https://www.patreon.com/namazon/shop'
 
 export const LINKS_NC_CLIPS: Record<string, string> = {
-   'NC45': '',
-   'NC46': '',
-   'NC47': '',
-   'NC48': '',
-   'NC49': '',
+   'NC45': 'https://www.patreon.com/namazon/shop/nc45-braivs-vs-alyona-13-10-2016-294185',
+   'NC46': 'https://www.patreon.com/namazon/shop/nc46-alena-kurmandi-30-03-2017-06-04-294096',
+   'NC47': 'https://www.patreon.com/namazon/shop/nc47-competitions-at-maslenitsa-16-02-294121',
+   'NC48': 'https://www.patreon.com/namazon/shop/nc48-competitions-spring-2017-294141',
+   'NC49': 'https://www.patreon.com/namazon/shop/nc49-braivs-vs-alyona-collection-2016-293193',
    'NC50': 'https://www.patreon.com/namazon/shop/nc50-braivs-vs-tais-collection-2014-2017-290070',
    'NC51': 'https://www.patreon.com/namazon/shop/nc51-kara-darya-tais-2012-290104'
-}
-
-type LINKS_NC_CLIPS = {
-
 }
